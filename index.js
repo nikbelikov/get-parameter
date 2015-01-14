7f828d5d41fd51a12f01ed245538b4bc32189f09
Augustus Yuan <ayuan@evernote.com>
2015-01-14 14:39:34 -0800

    added option so that if no parameter specified, will return an object with all GET params as key-value pairs

--- index.js
+++ index.js
@@ -8,8 +8,13 @@
 // ------------------------------------------------------------
 
 module.exports = function getParameter(name){
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)'),
-      results = regex.exec(location.search);
-  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  var queryDict = {};
+  location.search.substr(1).split('&').forEach(function(item) {queryDict[item.split('=')[0]] = decodeURIComponent(item.split('=')[1])});
+  
+  // if name specified, return that specific get parameter
+  if (name) {
+    return queryDict.hasOwnProperty(name) ? decodeURIComponent(queryDict[name].replace(/\+/g, ' ')) : '';
+  }
+
+  return queryDict;
 }
